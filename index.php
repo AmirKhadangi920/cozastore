928cb2d91c92e2eefe22c4f0d3b6f34eb0f7d7eb
reza khorrami <rezakhorrami729@gmail.com>
2018-10-23 17:10:48 +0330

    edit line 50 index.html

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						Free shipping for standard order over $100
+						حمل و نقل رایگان برای سفارش استاندارد بیش از 100 دلار
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
