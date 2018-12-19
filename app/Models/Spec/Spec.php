a4473a80a26508c4d95404270c30ce45242466ce
Amir Khadangi <30316688+AmirKhadangi920@users.noreply.github.com>
2018-12-19 19:12:18 +0330

    Create fully specification table panel

--- app/Models/Spec/Spec.php
+++ app/Models/Spec/Spec.php
@@ -3,9 +3,17 @@
 namespace App\Models\Spec;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Spec extends Model
 {
+    protected $fillable = [ 'category_id' ];
+
+    public function category ()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function specHeaders ()
     {
         return $this->hasMany(SpecHeader::class);
